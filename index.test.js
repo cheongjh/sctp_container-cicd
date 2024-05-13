09b0f88599f8636b5e38cd34d0d11edf371a92d2
unknown <cheongjh@gmail.com>
2024-05-13 21:34:38 +0800

    Change Dockerfile port

--- index.test.js
+++ index.test.js
@@ -1,16 +1,10 @@
-const { handler } = require('./index')
+const request = require('supertest');
+const app = require('./index');
 
-describe('handler', () => {
-  it('should return status code 200 and the correct message', async () => {
-    const event = {};
-    const expectedResponse = {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
-      }, null, 2),
-    };
-    const result = await handler(event);
-    expect(result).toEqual(expectedResponse);
+describe('GET /', () => {
+  it('responds with "Hello, world!"', async () => {
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Hello, world!');
   });
 });
